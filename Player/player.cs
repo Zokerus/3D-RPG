e70f51226aff273507fba30ec3258838f9e19718
Zokerus <Zokerus@users.noreply.github.com>
2023-12-07 06:02:33 +0100

    player movement based on camera looking direction
    TODO: rotate character into moving direction

--- Player/player.cs
+++ Player/player.cs
@@ -3,47 +3,59 @@ using System;
 
 public partial class player : CharacterBody3D
 {
+	[Export]
+	public camera_controller mainCamera;
+
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-    public override void _UnhandledInput(InputEvent @event)
+    public override void _Ready()
     {
-        Vector3 velocity = Velocity;
-
-        // Handle Jump.
-        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-            velocity.Y = JumpVelocity;
-
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
-        Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
-        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-        if (direction != Vector3.Zero)
-        {
-            velocity.X = direction.X * Speed;
-            velocity.Z = direction.Z * Speed;
+        if (mainCamera == null) 
+		{
+            GetTree().Quit();
         }
-        else
-        {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-        }
-
-        Velocity = velocity;
     }
 
-    public override void _PhysicsProcess(double delta)
+    public override void _UnhandledInput(InputEvent @event)
 	{
-        Vector3 velocity = Velocity;
+		Vector3 velocity = Velocity;
 
-        // Add the gravity.
-        if (!IsOnFloor())
-            velocity.Y -= gravity * (float)delta;
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+			velocity.Y = JumpVelocity;
 
-        Velocity = velocity;
-        MoveAndSlide();
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
+		Vector3 direction = (mainCamera.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+			rota
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
+
+		Velocity = velocity;
+		MoveAndSlide();
 	}
 }
