4ad10cc4c8b248035cf10ba1e674f0e81b7aef5a
Zokerus <Zokerus@users.noreply.github.com>
2023-12-17 13:23:08 +0100

    play first walk animation w/o moving

--- Player/player.cs
+++ Player/player.cs
@@ -7,12 +7,14 @@ public partial class player : CharacterBody3D
 	[Export]
 	public camera_controller mainCamera;
 
-	private const float Speed = 5.0f;
-	private const float characterRotationRate = 3*Mathf.Pi;
-	private const float JumpVelocity = 4.5f;
+	private AnimationTree m_animationTree;
+
+	private const float m_speed = 5.0f;
+	private const float m_characterRotationRate = 3*Mathf.Pi;
+	private const float m_jumpVelocity = 4.5f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	private float m_gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
     public override void _Ready()
     {
@@ -20,6 +22,8 @@ public partial class player : CharacterBody3D
 		{
             GetTree().Quit();
         }
+
+		m_animationTree = GetNode<AnimationTree>("AnimationTree");
     }
 
     public override void _UnhandledInput(InputEvent @event)
@@ -28,22 +32,23 @@ public partial class player : CharacterBody3D
 
 		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+			velocity.Y = m_jumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
-		Vector3 direction = (mainCamera.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
+		m_animationTree.Set("parameters/Locomotion/blend_position", inputDir);
+		//Vector3 direction = (mainCamera.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		//if (direction != Vector3.Zero)
+		//{
+		//	velocity.X = direction.X * m_speed;
+		//	velocity.Z = direction.Z * m_speed;
+		//}
+		//else
+		//{
+		//	velocity.X = Mathf.MoveToward(Velocity.X, 0, m_speed);
+		//	velocity.Z = Mathf.MoveToward(Velocity.Z, 0, m_speed);
+		//}
 
 		Velocity = velocity;
 	}
@@ -54,12 +59,12 @@ public partial class player : CharacterBody3D
 
 		// Add the gravity.
 		if (!IsOnFloor())
-			velocity.Y -= gravity * (float)delta;
+			velocity.Y -= m_gravity * (float)delta;
 
 		if (velocity.X != 0.0f || velocity.Z != 0.0f)
 		{
 			float rotationAngle = this.Transform.Basis.Z.SignedAngleTo(mainCamera.Transform.Basis.Z, Vector3.Up);
-			this.RotateY(Mathf.Sign(rotationAngle) * Mathf.Min(characterRotationRate * (float)delta, Mathf.Abs(rotationAngle)));
+			this.RotateY(Mathf.Sign(rotationAngle) * Mathf.Min(m_characterRotationRate * (float)delta, Mathf.Abs(rotationAngle)));
 		}
 
         Velocity = velocity;
