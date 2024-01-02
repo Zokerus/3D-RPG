3b50ffc054ac3b7e747c582a302e495338ebfac8
Zokerus <Zokerus@users.noreply.github.com>
2024-01-02 11:53:22 +0100

    add badit script and display detection sector

---
+++ Enemies/Bandit/bandit.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public partial class bandit : CharacterBody3D
+{
+	[Export]
+	public float detectionAngle = 60.0f;
+	[Export]
+	public float detectionDistance = 10.0f;
+
+	private const float Speed = 5.0f;
+	private const float JumpVelocity = 4.5f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
+    public override void _Ready()
+    {
+		Draw3D draw = new Draw3D();
+		AddChild(draw);	
+		draw.sector(Vector3.Zero, new Vector3(0, 0, 1), detectionDistance, detectionAngle, Colors.Red);
+    }
+
+    public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+
+	private void LookForPlayer()
+	{
+
+	}
+}
